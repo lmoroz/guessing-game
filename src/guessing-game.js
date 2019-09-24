b08c04fe5d638c3e91393bf8e3b335d33d5c039b
Larisa Morozhnikova <larisa.moroz@gmail.com>
2019-09-25 04:54:09 +0800

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.start = min;
+        this.end = max;
     }
 
     guess() {
-
+        this.middle = Math.ceil((this.start + this.end) / 2);
+        return this.middle;
     }
 
     lower() {
-
+        this.end = this.middle;
     }
 
     greater() {
-
+        this.start = this.middle;
     }
 }
 
